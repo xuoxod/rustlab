452c4b5af4109b205042eaa7a57092ecb0754a77
rick xuoxod <xuoxod@gmail.com>
2024-01-01 11:09:34 -0500

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
